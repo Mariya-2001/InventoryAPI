fc9026dc8c5e97118d1140212705987df0505ea9
Mariya <lamki01@hotmail.com>
2026-06-23 10:34:16 +0400

    file ResourceNotFoundException.java added

--- src/main/java/com/mariya/inventory/exception/ResourceNotFoundException.java
+++ src/main/java/com/mariya/inventory/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.mariya.inventory.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
